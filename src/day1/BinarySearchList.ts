83dc4cedacdbcfe9064ac421bf262b7ad24e47fd
amanuel <amanuel0000@gmail.com>
2023-03-08 12:36:13 +0300

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    // set low and high
+    let low = 0, high = haystack.length;
 
+    do {
+        //  set mid point 
+        let mid = Math.floor(low + (high - low) / 2)
+        const v= haystack[mid]
+        
+        if (v === needle) return true
+        else if (v > needle) {
+           high = mid;
+        }
+        else {
+            low = mid + 1
+        }
+    } while (low < high);
+
+
+    return false
 }
