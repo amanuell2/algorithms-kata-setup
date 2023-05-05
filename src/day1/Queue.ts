2edd6266dcbe6be2cc8efe7aab6bc1e38bda0144
amanuel tadesse <amanueltadesse@amanuels-MacBook-Pro.local>
2023-05-05 16:06:21 +0300

    implement queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,44 @@
+type QNode<T> = {
+    value: T;
+    next?: QNode<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: QNode<T>;
+    private tail?: QNode<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as QNode<T>;
+        this.length++;
 
-}
-    deque(): T | undefined {
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
 
-}
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    dequeue(): T | undefined {
+        if (!this.head) return undefined;
+
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        if (!this.head) this.tail = undefined;
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
